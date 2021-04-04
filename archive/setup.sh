4148bb2e8175b08e654a7e68da2015ec3401f628
Stan Chen <stanleychen587@gmail.com>
2021-04-04 02:11:32 -0600

    added req txt cred karen
    clean up

--- archive/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-pip install jupyterlab
-pip install pandas matplotlib seaborn pandas_datareader sklearn numpy
-pip install keras tensorflow
-pip install plotly
-pip install plotly --upgrade
-pip install finnhub-python
---
+++ setup.py
@@ -0,0 +1,29 @@
+# example usage
+# pip install git+https://github.com/deeptendies/deeptendies#egg=deeptendies
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="deeptendies",  # Replace with your own username
+    version="0.0.1-dev",
+    author="stancsz, mklasby, hasnil",
+    author_email="deeptendies@deeptendies.github.io",
+    description="deeptendies",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/deeptendies/deeptendies",
+    project_urls={
+        "Bug Tracker": "https://github.com/deeptendies/deeptendies/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "deeptendies"},
+    packages=setuptools.find_packages(where="deeptendies"),
+    python_requires=">=3.6",
+)
