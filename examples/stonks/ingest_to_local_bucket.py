05e2b981d4407e8eb32e5795254c16d403fd2c0a
Stan Chen <stanleychen587@gmail.com>
2021-04-10 15:11:45 -0600

    renamed codes, depreciated old code

--- examples/stonks/ingest_to_local_bucket.py
+++ examples/stonks/ingest_to_local_bucket.py
@@ -1,16 +1,28 @@
 import os
 import pathlib
-from datetime import date
+import random
 
-from deeptendies.stock_data import StockData
+import numpy as np
+from datetime import date
+from pandas_datareader import data
+from deeptendies.utils.local_bucket import save_data
+
+# ingest data for final report
 
 today = date.today()
+start_date = '2011-04-01'
+end_date = '2021-04-01'
 
-# get api keys from here https://finnhub.io/dashboard
-# if dev or deployment, save a `secrets.yaml` in work dir
-# secrets.yaml example: https://raw.githubusercontent.com/deeptendies/deeptendies/master/secrets.yaml.example
-ticker = 'GME'
-days = 180
+stonks = ["TSLA", "AAPL", "MSFT", "BA", "KO"]
 
-
-
+# load data with yahoo / data reader
+for ticker in stonks:
+    print(ticker, start_date, end_date)
+    candle_stick_data = data.DataReader(ticker, 'yahoo', start_date, end_date)
+    print(candle_stick_data)
+    save_data(dataframe=candle_stick_data,
+              bucket='fs',
+              topic=ticker,
+              version='demo',
+              suffix=f"{start_date}_to_{end_date}",
+              path="/home/stan/github/deeptendies")
