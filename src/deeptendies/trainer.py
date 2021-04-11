dcf443c3d6cd00978861359bddcc9017369c2708
KBehairy <64875612+KBehairy@users.noreply.github.com>
2021-04-11 17:08:17 -0600

    fix model training and graph loss vs epochs

--- src/deeptendies/trainer.py
+++ src/deeptendies/trainer.py
@@ -11,20 +11,6 @@ from tensorflow.keras.layers.experimental.preprocessing import Normalization
 class Trainer():
     """Trainer for model. 
     """
-#     def __init__(self, save_location, model, model_name):
-#         self.dir_location = save_location
-#         self.model = model
-#         self.model_name = model_name
-
-#     def split_data(self, X_dev, Y_dev, train_size):
-#         print("SPLITING DATA SET")
-#         self.X_train, self.X_val, self.Y_train, self.Y_val = train_test_split(X_dev, Y_dev, train_size=train_size)
-
-#     def train_model(self, batch_size, epochs, verbose, callbacks):
-#         print("TRAINING")
-#         self.model.fit(self.X_train, self.Y_train, batch_size, epochs, verbose, callbacks, validation_data=(self.X_val, self.Y_val))
-#         self.graph_results()
-        
     def __init__(self, save_location, model, model_name, train_data, test_data):
         self.dir_location = save_location
         self.model = model
@@ -34,22 +20,15 @@ class Trainer():
 
     def train_model(self, epochs, verbose, callbacks ):
         print("TRAINING")
-        self.model.fit(self.train_data, steps_per_epoch=len(self.train_data), epochs=epochs, verbose=verbose, callbacks=callbacks)
-        self.graph_results()
+        self.hist = self.model.fit(self.train_data, steps_per_epoch=len(self.train_data), epochs=epochs, verbose=verbose, callbacks=callbacks)
+        self.graph_loss()
 
-    def graph_results(self):
-        history = self.model.history
-        train_acc = history.history['accuracy']
-        val_acc = history.history['val_accuracy']
-
-        fig, ax = plt.subplots(figsize=(18,12))
-        train_ax = ax.scatter(x=range(len(train_acc)), y=train_acc, label="Training Accuracy")
-        val_ax = ax.scatter(x=range(len(train_acc)), y=val_acc, label="Validation Accuracy")
-        legend = ax.legend()
-        fig.suptitle("Min/Max Normalized FCNN Accuracy vs. Epoch")
+    def graph_loss(self):
+        loss = self.hist.history['loss']
+        plt.plot(self.hist.epoch, loss)
+        plt.title("Loss vs. Epoch")
         plt.xlabel("Epoch")
-        plt.ylabel("Accuracy")
-        fig.show()
+        plt.ylabel("Loss")
 
     def save_model_collab(self):
         """INTENDED FOR COLLAB ONLY"""
