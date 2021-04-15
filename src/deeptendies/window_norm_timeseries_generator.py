aab83a81a43dfa947d57878f791425aed9046156
Mike Lasby <mklasby@ucalgary.ca>
2021-04-14 22:59:25 -0600

    adding target idx instead of hard coded 3 to windowed norm generator

--- src/deeptendies/window_norm_timeseries_generator.py
+++ src/deeptendies/window_norm_timeseries_generator.py
@@ -136,7 +136,7 @@ class WindowNormTimeseriesGenerator(TimeseriesGenerator):
       samples = np.array([self.data[row - self.length:row:self.sampling_rate]
                           for row in rows])
       if mask: 
-          samples[0][-mask_idx:][:,target_idx] = np.repeat(samples[0][self.length-mask_idx-1][3], mask_idx)
+          samples[0][-mask_idx:][:,target_idx] = np.repeat(samples[0][self.length-mask_idx-1][target_idx], mask_idx)
 
       targets = np.array([self.targets[row] for row in rows])
 
