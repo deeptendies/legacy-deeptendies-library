d6b90373d6621ce083dd7da78c81ce07bdb3e863
Stan Chen <stanleychen587@gmail.com>
2021-05-27 23:34:43 -0600

    updated default main usage

--- main.py
+++ main.py
@@ -3,20 +3,24 @@ from dtlite.model import train_and_predict
 
 
 def run_forecast(stock='gme',
-                 start='2021-01-01',
-                 end='2021-01-30',
+                 start='2021-05-10',
+                 end='2021-05-27',
                  forecast_days=5,
                  *args, **kwargs):
     df = ingest_yahoo_finance(
-        stock='gme',
-        start='2021-05-10',
-        end='2021-05-26',
-        forecast_days=5,
+        stock=stock,
+        start=start,
+        end=end,
+        forecast_days=forecast_days,
         filter=['High']
     )
     forecast = train_and_predict(data=df)
-    print(forecast)
+    # print(forecast)
+    return forecast
 
 
 if __name__ == '__main__':
-    run_forecast()
+    stocks=['gme', 'amc', 'cour', 'sret']
+    for i in stocks:
+        fc=run_forecast(i)
+        print(i, fc)
